930ab84ee6973c7b1aaf5e0460bf6236c2f70a0a
Adrian Youda Liminata <149176847+tayonade@users.noreply.github.com>
2025-12-06 14:07:10 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
+import 'package:envi/pages/control_page.dart';
 import 'package:flutter/material.dart';
-import 'package:skibiditoiltetv2/screens/homeScreen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -8,15 +8,13 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'My Flutter App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      darkTheme: ThemeData.dark(),
-      home: const AirQualityApp(),
+      theme: ThemeData.dark(),
+      debugShowCheckedModeBanner: false,
+      home: controlPage(),
     );
   }
 }
